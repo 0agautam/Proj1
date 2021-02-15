7ae1afb02370ef744f1dfffa5a3644ae2c6c49fd
Ankit Gautam <0agautam@gmail.com>
2021-02-15 17:34:38 +0530

    Changed math_ and class_sample files

--- ruby/class_sample.rb
+++ ruby/class_sample.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/ruby -w
 
 class Food
+attr_accessor :protein
   def initialize(protein)
     @protein = protein
+    puts @protein
   end
 end
 bacon = Food.new(21)
-bacon::@protein
+bacon.protein
 
--- ruby/math_.rb
+++ ruby/math_.rb
@@ -4,13 +4,20 @@ module Math_
   PI = 3.14
   ZERO =0
 
-  def additon(a,b)
+  def Math_.addition(a,b)
     return a+b
   end
 
-  def subtraction(a-b)
+  def Math_.subtraction(a,b)
     return a-b
   end
   
+  def Math_.multiplication(a,b)
+    return a*b
+  end
+
+  def Math_.division(a,b)
+    return a/b
+  end
 end
 
