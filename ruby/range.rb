9afa8e9fb21474370d571fd8cf44cb83926614a1
Ankit Gautam <0agautam@gmail.com>
2021-02-15 17:32:45 +0530

    Getting as many inputs using range

---
+++ ruby/range.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby -w
+#Program to get input untill you type start input with 'start' and then end with 'end'
+
+def range_
+  while gets
+
+    print if /start/../end/
+  end
+end
+
+
+
+range_
